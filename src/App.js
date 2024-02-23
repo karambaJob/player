282f062056295658c6dfc1785dea9ebb9628fca1
karambaJob <gdim2010@gmail.com>
2024-02-23 16:08:33 +0700

    feaat: relative path, prettier, content

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
-import React, { useState } from 'react';
-import ReactPlayer from 'react-player';
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+
+const DATA = [
+  {
+    url: "https://www.youtube.com/watch?v=ezb55xSinoY",
+    title: "Машинки бип бип",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=LbOve_UZZ54",
+    title: "Синий трактор - По полям",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=0tSBxGHcaFA",
+    title: "Акуленок ту-ру-ру-ру-ру",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=4iHQev6oHEo",
+    title: "Тракторенок ту-ру-ру-ру-ру",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=1TYl3jhfEDA",
+    title: "Маша и медведь - мультик про хрюшку",
+  },
+
+  {
+    url: "https://www.youtube.com/watch?v=lr28F6E3H_o",
+    title: "смешарики нюша на велосипеде",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=1WyUN66MMlc",
+    title: "смешарики лосяш и бутерброд",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=XkOiBQfiqTU",
+    title: "байки мэтра - про летающую тарелку",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=dA5wFDP1SkQ&list=PL8XzBNh9xhcz2XwhGg7etnDGZHXRvRhId&index=5",
+    title: "Бибика про большие машины",
+    img: "https://i.ytimg.com/vi/dA5wFDP1SkQ/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCv1YfhbvIPaTA20c7qvufm4NYF3w",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=vLJahVX2nx0&t",
+    title: "Бибика про погоду",
+    img: "https://i.ytimg.com/vi/vLJahVX2nx0/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLD-u4P5CT2OM7wZwAkAmOy3kHPRow",
+  },
+];
 
 const VideoPlayer = () => {
-  const [videos, setVideos] = useState([
-    'https://www.youtube.com/watch?v=ezb55xSinoY',
-    'https://www.youtube.com/watch?v=6JN0ZGJf5M8',
-    'https://www.youtube.com/watch?v=9bZkp7q19f0',
-    'https://www.youtube.com/watch?v=LbOve_UZZ54',
-    'https://www.youtube.com/watch?v=0tSBxGHcaFA',
-    'https://www.youtube.com/watch?v=4iHQev6oHEo'
-  ]);
-
   const [playing, setPlaying] = useState(true);
   const [activeIndex, setActiveIndex] = useState(0);
- 
+
   const handleVideoClick = (index) => {
     setActiveIndex(index);
     setPlaying(true);
@@ -24,18 +61,34 @@ const VideoPlayer = () => {
   };
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column', height: '100vh'}}>
-      <div style={{ marginBottom: '20px', flexGrow: 1 }}>
-        <ReactPlayer url={videos[activeIndex]} playing={playing} controls={false} width="100%" height="100%" />
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <div style={{ marginBottom: "20px", flexGrow: 1 }}>
+        <ReactPlayer
+          url={DATA[activeIndex].url}
+          playing={playing}
+          controls={false}
+          width="100%"
+          height="100%"
+        />
       </div>
-      <div style={{height: '40px'}}>
-          <button style={{height: '100%', marginRight: '20px'}} onClick={handleStop}>Стоп</button>
-          <button style={{height: '100%'}} onClick={() => {
+      <div style={{ height: "40px", display: "none" }}>
+        <button
+          style={{ height: "100%", marginRight: "20px" }}
+          onClick={handleStop}
+        >
+          Стоп
+        </button>
+        <button
+          style={{ height: "100%" }}
+          onClick={() => {
             setPlaying(true);
-          }}>Играй</button>
+          }}
+        >
+          Играй
+        </button>
       </div>
-      <div style={{height: '150px'}}>
-      <VideoThumbnail videos={videos} onVideoClick={handleVideoClick} />
+      <div style={{ height: "150px" }}>
+        <VideoThumbnail videos={DATA} onVideoClick={handleVideoClick} />
       </div>
     </div>
   );
@@ -43,15 +96,17 @@ const VideoPlayer = () => {
 
 export default VideoPlayer;
 
-
 const VideoThumbnail = ({ videos, onVideoClick }) => {
   return (
-    <div style={{ display: 'flex', overflowX: 'auto', height: '100%' }}>
+    <div style={{ display: "flex", overflowX: "auto", height: "100%" }}>
       {videos.map((video, index) => (
         <img
           key={index}
-          src={`https://img.youtube.com/vi/${video.split('=')[1]}/0.jpg`}
-          style={{ marginRight: '10px', cursor: 'pointer' }}
+          src={
+            video.img ||
+            `https://img.youtube.com/vi/${video.url.split("=")[1]}/0.jpg`
+          }
+          style={{ marginRight: "10px", cursor: "pointer" }}
           onClick={() => onVideoClick(index)}
         />
       ))}
