846b910736b65365b1f59ca763d1f96354fadee0
karambaJob <gdim2010@gmail.com>
2024-03-02 15:58:28 +0700

    feat: many style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,230 @@ import React, { useState, useRef, useEffect } from "react";
 import { act } from "react-dom/test-utils";
 import ReactPlayer from "react-player";
 
+import "./App.css";
+
+function preventDefault(e) {
+  console.log("test");
+  // e.preventDefault();
+}
+
+let tId = null;
+
+const VideoPlayer = () => {
+  const [isUserActive, setUserActive] = useState(false);
+  const [playing, setPlaying] = useState(true);
+  const [hoverTag, setHover] = useState(0);
+  const [activeTag, setActiveTag] = useState(-1);
+  const [lastTitle, setLastTitle] = useState("");
+  const [activeVideoIndex, setActiveVideoIndex] = useState(0);
+  const [hoverVideoIndex, sethoverVideoIndex] = useState(0);
+  const CURRENT_DATA = DATA.filter((item) => {
+    return activeTag === 0
+      ? !item.tags || item.tags.includes(activeTag)
+      : item.tags && item.tags.includes(activeTag);
+  });
+
+  const handleStop = () => {
+    setPlaying(false);
+  };
+
+  useEffect(() => {
+    document.body.addEventListener("touchstart", () => {
+      clearTimeout(tId);
+      setUserActive(true);
+      tId = setTimeout(() => {
+        setUserActive(false);
+      }, 5000);
+    });
+
+    document.body.addEventListener("touchmove", preventDefault, {
+      passive: false,
+    });
+  }, []);
+
+  return (
+    <div className={"main " + (isUserActive ? "" : "userUnactive")}>
+      {[...TOP_SCROLL, ...DATA].map((item) => (
+        <SoundPlayer name={item.title} startPlay={item.title === lastTitle} />
+      ))}
+      <div style={{ marginBottom: "20px", flexGrow: 1, position: "relative" }}>
+        <ReactPlayer
+          url={
+            CURRENT_DATA.length > 0
+              ? CURRENT_DATA[activeVideoIndex].url
+              : DATA[0].url
+          }
+          playing={playing}
+          controls={false}
+          width="100%"
+          height="100%"
+        />
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            background: "rgba(0, 0 ,0, 0)",
+            zIndex: 111,
+            display: "flex",
+            alignItems: "flex-end",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              background: playing ? "rgba(0, 0 ,0, 0)" : "green",
+              width: "100%",
+              height: "20%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <button
+              className={"play " + (playing ? "" : "paused")}
+              onClick={() => {
+                setPlaying(!playing);
+              }}
+            ></button>
+          </div>
+        </div>
+      </div>
+
+      <div className="controls">
+        {activeTag > -1 ? (
+          <>
+            <button
+              style={{
+                width: "150px",
+                display: "block",
+                flexShrink: 0,
+                backgroundSize: "cover",
+                backgroundImage: `url("https://png.pngtree.com/png-clipart/20190517/original/pngtree-vector-back-icon-png-image_4267356.jpg")`,
+              }}
+              onClick={() => {
+                setActiveTag(-1);
+              }}
+            ></button>
+            <ScrollList
+              items={CURRENT_DATA}
+              hoverIndex={hoverVideoIndex}
+              activeVideoIndex={activeVideoIndex}
+              onHover={(index) => {
+                sethoverVideoIndex(index);
+                setLastTitle(CURRENT_DATA[index].title);
+                handleStop();
+              }}
+              onActive={(index) => {
+                setActiveVideoIndex(index);
+                setPlaying(true);
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <ScrollList
+              items={TOP_SCROLL}
+              hoverIndex={hoverTag}
+              onHover={(index) => {
+                setHover(index);
+                setLastTitle(TOP_SCROLL[index].title);
+                handleStop();
+              }}
+              onActive={(index) => {
+                setActiveTag(index);
+                setActiveVideoIndex(0);
+                sethoverVideoIndex(0);
+              }}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default VideoPlayer;
+
+const ScrollList = ({
+  items,
+  onHover,
+  onActive,
+  hoverIndex,
+  activeVideoIndex,
+}) => {
+  console.log("activeVideoIndex: ", activeVideoIndex);
+  return (
+    <div
+      style={{
+        display: "flex",
+        overflowX: "auto",
+        height: "100%",
+      }}
+    >
+      {items.map((item, index) => {
+        let className = "preview";
+
+        if (index === activeVideoIndex) {
+          className += " selected";
+        } else if (index === hoverIndex) {
+          className += " active";
+        }
+        return (
+          <img
+            key={index}
+            src={
+              item.img ||
+              `https://img.youtube.com/vi/${item.url.split("=")[1]}/0.jpg`
+            }
+            className={className}
+            onClick={() => {
+              onHover(index);
+              if (index === hoverIndex) {
+                onActive(index);
+              }
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+const SoundPlayer = ({ name, startPlay = false }) => {
+  const audioRef = useRef(null);
+  const [canPlay, setCanPlay] = useState(false);
+
+  const loadSound = () => {
+    audioRef.current.src = process.env.PUBLIC_URL + `/audio/${name}.mp3`;
+  };
+
+  useEffect(() => {
+    loadSound();
+  }, [name]);
+
+  useEffect(() => {
+    if (startPlay && canPlay) {
+      audioRef.current.play();
+    }
+  }, [startPlay]);
+
+  return (
+    <div>
+      <audio
+        ref={audioRef}
+        onCanPlay={() => {
+          setCanPlay(true);
+        }}
+        controls
+        style={{ display: "none" }}
+      />
+    </div>
+  );
+};
+
 const DATA = [
   {
     url: "https://www.youtube.com/watch?v=ezb55xSinoY",
@@ -173,210 +397,3 @@ const TOP_SCROLL = [
     img: "https://i.ytimg.com/vi/g8AVQXoYWs4/maxresdefault.jpg",
   },
 ];
-
-function preventDefault(e) {
-  e.preventDefault();
-}
-
-const VideoPlayer = () => {
-  const [playing, setPlaying] = useState(true);
-  const [hoverTag, setHover] = useState(0);
-  const [activeTag, setActiveTag] = useState(-1);
-  const [lastTitle, setLastTitle] = useState("");
-  const [activeVideoIndex, setActiveVideoIndex] = useState(0);
-  const [hoverVideoIndex, sethoverVideoIndex] = useState(0);
-  const CURRENT_DATA = DATA.filter((item) => {
-    return activeTag === 0
-      ? !item.tags || item.tags.includes(activeTag)
-      : item.tags && item.tags.includes(activeTag);
-  });
-
-  const handleStop = () => {
-    setPlaying(false);
-  };
-
-  useEffect(() => {
-    document.body.addEventListener("touchmove", preventDefault, {
-      passive: false,
-    });
-  }, []);
-
-  return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-      {[...TOP_SCROLL, ...DATA].map((item) => (
-        <SoundPlayer name={item.title} startPlay={item.title === lastTitle} />
-      ))}
-      <div style={{ marginBottom: "20px", flexGrow: 1, position: "relative" }}>
-        <ReactPlayer
-          url={
-            CURRENT_DATA.length > 0
-              ? CURRENT_DATA[activeVideoIndex].url
-              : DATA[0].url
-          }
-          playing={playing}
-          controls={false}
-          width="100%"
-          height="100%"
-        />
-        <div
-          style={{
-            position: "absolute",
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            background: "rgba(0, 0 ,0, 0)",
-            zIndex: 111,
-            display: "flex",
-            alignItems: "flex-end",
-            justifyContent: "center",
-          }}
-        >
-          <div
-            style={{
-              background: playing ? "rgba(0, 0 ,0, 0)" : "green",
-              width: "100%",
-              height: "20%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <button
-              style={{ height: "50px", width: "150px" }}
-              onClick={() => {
-                setPlaying(true);
-              }}
-            >
-              Играй
-            </button>
-            <button
-              style={{ height: "50px", marginLeft: "20px", width: "150px" }}
-              onClick={handleStop}
-            >
-              Стоп
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div style={{ height: "150px", display: "flex" }}>
-        {activeTag > -1 ? (
-          <>
-            <button
-              style={{
-                width: "150px",
-                display: "block",
-                flexShrink: 0,
-                backgroundSize: "cover",
-                backgroundImage: `url("https://png.pngtree.com/png-clipart/20190517/original/pngtree-vector-back-icon-png-image_4267356.jpg")`,
-              }}
-              onClick={() => {
-                setActiveTag(-1);
-              }}
-            ></button>
-            <ScrollList
-              items={CURRENT_DATA}
-              hoverIndex={hoverVideoIndex}
-              onHover={(index) => {
-                sethoverVideoIndex(index);
-                setLastTitle(CURRENT_DATA[index].title);
-                handleStop();
-              }}
-              onActive={(index) => {
-                setActiveVideoIndex(index);
-                setPlaying(true);
-              }}
-            />
-          </>
-        ) : (
-          <>
-            <ScrollList
-              items={TOP_SCROLL}
-              hoverIndex={hoverTag}
-              onHover={(index) => {
-                setHover(index);
-                setLastTitle(TOP_SCROLL[index].title);
-                handleStop();
-              }}
-              onActive={(index) => {
-                setActiveTag(index);
-                setActiveVideoIndex(0);
-                sethoverVideoIndex(0);
-              }}
-            />
-          </>
-        )}
-      </div>
-    </div>
-  );
-};
-
-export default VideoPlayer;
-
-const ScrollList = ({ items, onHover, onActive, hoverIndex }) => {
-  return (
-    <div
-      style={{
-        display: "flex",
-        overflowX: "auto",
-        height: "100%",
-      }}
-    >
-      {items.map((item, index) => (
-        <>
-          <img
-            key={index}
-            src={
-              item.img ||
-              `https://img.youtube.com/vi/${item.url.split("=")[1]}/0.jpg`
-            }
-            style={{
-              cursor: "pointer",
-              border:
-                index === hoverIndex ? "10px solid green" : "20px solid black",
-            }}
-            onClick={() => {
-              onHover(index);
-              if (index === hoverIndex) {
-                onActive(index);
-              }
-            }}
-          />
-        </>
-      ))}
-    </div>
-  );
-};
-
-const SoundPlayer = ({ name, startPlay = false }) => {
-  const audioRef = useRef(null);
-  const [canPlay, setCanPlay] = useState(false);
-
-  const loadSound = () => {
-    audioRef.current.src = process.env.PUBLIC_URL + `/audio/${name}.mp3`;
-  };
-
-  useEffect(() => {
-    loadSound();
-  }, [name]);
-
-  useEffect(() => {
-    if (startPlay && canPlay) {
-      audioRef.current.play();
-    }
-  }, [startPlay]);
-
-  return (
-    <div>
-      <audio
-        ref={audioRef}
-        onCanPlay={() => {
-          setCanPlay(true);
-        }}
-        controls
-        style={{ display: "none" }}
-      />
-    </div>
-  );
-};
